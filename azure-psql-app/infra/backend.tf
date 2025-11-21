0849c0b396445558ed7a8e25192254aa8fe66f8f
Hubert Kozuchowski <hubertkozuchowski1337@gmail.com>
2025-11-21 08:58:45 +0100

    fix: remove duplicate azure-psql-app directory from repository

--- azure-psql-app/infra/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "notesapp-dev-rg"
-    storage_account_name = "tfstatenotesapp"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-  }
-}
